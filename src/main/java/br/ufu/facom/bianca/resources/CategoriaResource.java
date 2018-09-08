3af53ce89425f5deda2630d61121c35313b007e2
Bianca Cristina da Silva <phon-bia@hotmail.com>
2018-09-08 13:31:21 -0300

    Criacao classe Categoria e CategoriaResource

---
+++ src/main/java/br/ufu/facom/bianca/resources/CategoriaResource.java
@@ -0,0 +1,27 @@
+package br.ufu.facom.bianca.resources;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.ufu.facom.bianca.domain.Categoria;
+
+@RestController
+@RequestMapping(value="/categorias")
+public class CategoriaResource {
+	
+	@RequestMapping(method= RequestMethod.GET)
+	public List<Categoria> listar() {
+		Categoria cat1 = new Categoria(1, "Romance");
+		Categoria cat2 = new Categoria(2,"Ficção Científica");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista; 
+	}
+}
