d8a578b5f9f65030cc77f73b63d7edc87d8711c5
Bianca Cristina da Silva <phon-bia@hotmail.com>
2018-09-08 13:01:35 -0300

    Criacao do projeto

---
+++ src/main/java/br/ufu/facom/bianca/MinhaEstanteBackendApplication.java
@@ -0,0 +1,12 @@
+package br.ufu.facom.bianca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinhaEstanteBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinhaEstanteBackendApplication.class, args);
+	}
+}
