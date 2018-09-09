cc58f0b57f925cf130d7cd3380dc87d15045caef
Bianca Cristina da Silva <phon-bia@hotmail.com>
2018-09-09 14:52:32 -0300

    Metodo POST para Categoria, modificacao em CategoriaResource e CategoriaService

--- src/main/java/br/ufu/facom/bianca/resources/CategoriaResource.java
+++ src/main/java/br/ufu/facom/bianca/resources/CategoriaResource.java
@@ -1,13 +1,20 @@
 package br.ufu.facom.bianca.resources;
 
+import java.net.URI;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.ufu.facom.bianca.domain.Categoria;
+import br.ufu.facom.bianca.dto.CategoriaDTO;
 import br.ufu.facom.bianca.services.CategoriaService;
 
 @RestController
@@ -15,7 +22,7 @@ import br.ufu.facom.bianca.services.CategoriaService;
 public class CategoriaResource {
 	
 	@Autowired
-	private CategoriaService categoriaService;
+	private CategoriaService service;
 	
 	@RequestMapping(value="/{id}", method= RequestMethod.GET)
 	public ResponseEntity<?> find(@PathVariable Integer id) {
@@ -23,7 +30,20 @@ public class CategoriaResource {
 		// O "?" eh porque a resposta pode ser bem sucedida ou nao
 		// ResponseEntity = armazena infos da resposta HTTP para o servico REST
 		
-		Categoria obj = categoriaService.findById(id);
+		Categoria obj = service.find(id);
 		return ResponseEntity.ok().body(obj); // Retorna uma resposta OK e o objeto como corpo
+	}
+	
+	@RequestMapping(method=RequestMethod.POST) // Faz com que seja um metodo POST
+	public ResponseEntity<Void> insert(@Valid @RequestBody CategoriaDTO objDTO) {
+		//@RequestBody = faz o Json ser convertido automaticamente para o objeto java
+		Categoria obj = service.fromDTO(objDTO); // Converte CategoriaDTO em Categoria
+		obj = service.insert(obj); // Chama o metodo "insert" do objeto "service" que eh do tipo CategoriaService
+		// Http status code = mostra os codigos http padrao 
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		// Esse uri serve pra me dar a url da nova categoria que inseri
+		
+		return ResponseEntity.created(uri).build();		
 	}
 }
--- src/main/java/br/ufu/facom/bianca/services/CategoriaService.java
+++ src/main/java/br/ufu/facom/bianca/services/CategoriaService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.ufu.facom.bianca.domain.Categoria;
+import br.ufu.facom.bianca.dto.CategoriaDTO;
 import br.ufu.facom.bianca.repositories.CategoriaRepository;
 import br.ufu.facom.bianca.resources.exceptions.ObjectNotFoundException;
 
@@ -15,7 +16,7 @@ public class CategoriaService {
 	@Autowired
 	private CategoriaRepository repo;
 	
-	public Categoria findById (Integer id) {
+	public Categoria find(Integer id) {
 		// Esse metodo busca uma categoria a partir de seu ID
 		
 		Optional<Categoria> obj = repo.findById(id);
@@ -23,5 +24,17 @@ public class CategoriaService {
 		// O retorno abaixo trata o erro de caso o obj nao tenha sido encontrado usando o tratamento de erro criado
 		return obj.orElseThrow( () -> new ObjectNotFoundException(
 				"Objeto nao encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+	}
+
+	public Categoria insert (Categoria obj) {
+		obj.setId(null);
+		// Se esse obj tiver algum ID, o metodo save vai considerar que eh uma atualizacao e nao uma insercao
+		// Logo, o comando acima garante que estou inserindo um objeto do tipo Categoria com ID nulo
+		return repo.save(obj);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDTO) {
+		// Metodo auxiliar que instancia um objeto do tipo Categoria a partir de um objeto do tipo CategoriaDTO
+		return new Categoria(objDTO.getId(),objDTO.getNome());
 	}
 }
