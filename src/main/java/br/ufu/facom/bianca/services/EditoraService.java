b3a3ac39ad6dbd4562e656d5a83840f198da2b06
Bianca Cristina da Silva <phon-bia@hotmail.com>
2018-09-09 19:49:26 -0300

    Metodo POST para Editora, modificacao em EditoraResource e EditoraService

--- src/main/java/br/ufu/facom/bianca/services/EditoraService.java
+++ src/main/java/br/ufu/facom/bianca/services/EditoraService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.ufu.facom.bianca.domain.Editora;
+import br.ufu.facom.bianca.dto.EditoraDTO;
 import br.ufu.facom.bianca.repositories.EditoraRepository;
 import br.ufu.facom.bianca.resources.exceptions.ObjectNotFoundException;
 
@@ -23,5 +24,17 @@ public class EditoraService {
 		// O retorno abaixo trata o erro de caso o obj nao tenha sido encontrado usando o tratamento de erro criado
 		return obj.orElseThrow( () -> new ObjectNotFoundException(
 				"Objeto nao encontrado! Id: " + id + ", Tipo: " + Editora.class.getName()));
+	}
+	
+	public Editora insert (Editora obj) {
+		// Metodo que insere uma nova Editora 
+		obj.setId(null);
+
+		return repo.save(obj);
+	}	
+	
+	public Editora fromDTO(EditoraDTO objDTO) {
+		// Metodo auxiliar que instancia um objeto do tipo Editora a partir de um objeto do tipo EditoraDTO
+		return new Editora(objDTO.getId(),objDTO.getNome());
 	}
 }
