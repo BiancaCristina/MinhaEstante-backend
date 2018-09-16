80bef740e300a294a4aa28b3828ff7bf16781aa0
Bianca Cristina da Silva <phon-bia@hotmail.com>
2018-09-16 19:46:02 -0300

    Validacao personalizada

--- src/main/java/br/ufu/facom/bianca/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/br/ufu/facom/bianca/resources/exceptions/ResourceExceptionHandler.java
@@ -4,6 +4,8 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -17,5 +19,19 @@ public class ResourceExceptionHandler {
 		// HttpStatus.NOT_Found = error 404
 		
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+	}
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<StandardError> validation(MethodArgumentNotValidException e, HttpServletRequest request){
+		ValidationError err = new ValidationError(HttpStatus.UNPROCESSABLE_ENTITY.value(), "Erro de validacao",System.currentTimeMillis());
+		// HttpStatus.BAD_REQUEST = error 400
+		
+		// Percorre a lista de erros do "MethodArgumentNotValidException" e pega apenas a mensagem do erro
+		for(FieldError x: e.getBindingResult().getFieldErrors()) // Para cada objeto x (do tipo Field Error) da lista "e", faco:
+		{
+			err.addError(x.getField(), x.getDefaultMessage());
+		}
+		
+		return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body(err);
 	}
 }
